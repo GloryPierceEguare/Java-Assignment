21955ecb81bf9cd801ec40ef377f4d245c7964ea
GloryPierceEguare <c17332081mydit.ie>
2019-03-26 22:18:01 +0000

    Remove punctuation from entered string

--- Assignment_1/src/com/Assignment_1/test/SentenceReader.java
+++ Assignment_1/src/com/Assignment_1/test/SentenceReader.java
@@ -21,23 +21,21 @@ public class SentenceReader
 	public void addSentence(String sentence) 
 	{
 	    //GUI.Passer(sentence);
-		String sentence2 = sentence.toLowerCase(); 
-	    System.out.println(sentence2);
-	    String[] a1 = sentence2.split(" ");    
+		//String sentence2 = sentence.toLowerCase(); 
+	    System.out.println(sentence);
+	    String[] a1 = sentence.split(" ");    
 	    
 	    File fileName = new File("English.txt");
 	    //File fileName2 = new File("BadWords.txt");
 	    
 	    try 
-	    {
-	        //Scanner myScanner = new Scanner(fileName);
-	        //Scanner myScanner2 = new Scanner(fileName2);
-	        
+	    {   
 	        //for(String sent : a1) 
 	        for(i=0; i<a1.length;i++)
 		    {
 	        	Scanner myScanner = new Scanner(fileName);
 		        //System.out.println(sent);
+	        	a1[i] = a1[i].replaceAll("[^a-zA-Z0-9]","").toLowerCase(); //https://stackoverflow.com/questions/22367028/given-a-txt-file-i-need-to-lower-case-and-remove-punctuation
 	        	System.out.println(a1[i]);
 	        	System.out.println("yep");
 		        
@@ -45,7 +43,6 @@ public class SentenceReader
 		        while (myScanner.hasNextLine()) 
 		        {
 		            String line = myScanner.nextLine();
-		            //String line2 = myScanner2.nextLine();
 		 
 		            //if(line.equals(sent)) 
 		            if(a1[i].equals(line))//(line.equals(a1[i]))
@@ -58,9 +55,6 @@ public class SentenceReader
 		        }
 		        myScanner.close();
 		    }     
-	        
-	        //myScanner.close();
-	        //myScanner2.close();
 	    } 
 	    catch(FileNotFoundException e) 
 	    { 
@@ -78,7 +72,6 @@ public class SentenceReader
 	    System.out.println("This string is: "+formalp+"% formal language.");
 	    System.out.println("This string is: "+slangp+"% informal language.");
 	    
-	    System.out.println("This string is: "+slangp+"% informal language.");
 	}
 	
 	//Close the scanner
